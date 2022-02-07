f81f9dd71e353fd803370c3105a5ac7c3131b116
Samuel Arguello <sam.rene12@gmail.com>
2022-02-07 10:46:15 -0600

    completed work in js-warmups

---
+++ js/js-warmups.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function doesJsUnderstand(input) {
+        return "I have no idea what you just said";
+}
+// console.log(doesJsUnderstand("hi"));
+
+function numTimesTwo(num) {
+    return num * 2;
+}
+// console.log(numTimesTwo(2));
+// console.log(numTimesTwo(100));
