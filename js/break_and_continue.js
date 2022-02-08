52769ddb42a7ca65ed21976a130a2fa7553165cf
Samuel Arguello <sam.rene12@gmail.com>
2022-02-08 12:06:53 -0600

    added aditional features to break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,11 +2,16 @@
 
 let oddNums = "Here is an odd number: ";
 let userNum = Number(prompt("Please enter an odd number between 1 and 50"));
+if(userNum % 2 === 0 || userNum > 50 || userNum < 0) {
+    alert("That is not an odd number between 1 and 50!")
+} else {
 
-for(let i = 1; i <= 50; i += 2){
-    if(userNum === i) {
-        console.log("Yikes, skipping this number: " + userNum);
-        continue;
+    for (let i = 1; i <= 50; i += 2) {
+        if (userNum === i) {
+            console.log("Yikes, skipping this number: " + userNum);
+            continue;
+        }
+        console.log(oddNums + i);
     }
-    console.log(oddNums + i);
+
 }
