d4f099a416521acf33fcf401e6d9b6d6b96711ef
Samuel Arguello <sam.rene12@gmail.com>
2022-02-07 21:29:08 -0600

    completed for loop exercises in for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,45 +1,45 @@
 "use strict";
 
-// function showMultiplicationTable(num) {
-//     for(let i = 1; i <= 10; i++) {
-//         let multiplication = num * i;
-//         console.log(num + " x " + i + " = " + multiplication);
-//     }
-// }
-// showMultiplicationTable(2)
-
-
-// function tenRandomEvenOrOdd(num) {
-//     for(let i = 1; i <= 10; i++) {
-//         let randomNum = Math.floor(Math.random() * 180) + 20
-//
-//         if(randomNum % 2 === 0) {
-//             console.log(randomNum + " is Even")
-//         } else {
-//             console.log(randomNum + " is Odd")
-//         }
-//     }
-// }
-// tenRandomEvenOrOdd();
-
-for (let x = 1; x < 10; x++) {
-for (let i = "1"; i.length < 10; i += "1"){
-    //console.log(i);
-        console.log(i * x);
+function showMultiplicationTable(num) {
+    for(let i = 1; i <= 10; i++) {
+        let multiplication = num * i;
+        console.log(num + " x " + i + " = " + multiplication);
     }
 }
+showMultiplicationTable(2)
 
-// for (let i = "1"; i.length < 10; i += "1"){
-//     console.log(i);
-//     for(let x = 1; x < 10; x ++){
-//         console.log(i * x);
-//     }
-// }
 
-// for(let i = 100; i >= 5; i--){
-//     if (i % 5 === 0) {
-//         console.log(i);
-//     }
-// }
+function tenRandomEvenOrOdd(num) {
+    for(let i = 1; i <= 10; i++) {
+        let randomNum = Math.floor(Math.random() * 180) + 20
+
+        if(randomNum % 2 === 0) {
+            console.log(randomNum + " is Even")
+        } else {
+            console.log(randomNum + " is Odd")
+        }
+    }
+}
+tenRandomEvenOrOdd();
+
+
+function numberPyramid() {
+    let output = "";
+    for (let x = 1; x <= 9; x++) {
+        for (let i = 1; i <= x; i++) {
+            output += x;
+        }
+        console.log(output);
+        output = "";
+    }
+}
+numberPyramid();
+
+
+for(let i = 100; i >= 5; i--){
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+}
 
 
