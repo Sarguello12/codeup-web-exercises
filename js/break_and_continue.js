15a0cf48311d17c08f4c728dc6f52af6e56b32f7
Samuel Arguello <sam.rene12@gmail.com>
2022-02-08 12:30:22 -0600

    refactored code in break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,17 +1,21 @@
 "use strict";
 
 let oddNums = "Here is an odd number: ";
-let userNum = Number(prompt("Please enter an odd number between 1 and 50"));
-if(userNum % 2 === 0 || userNum > 50 || userNum < 0) {
-    alert("That is not an odd number between 1 and 50!")
-} else {
+let userNum = prompt("Please enter an odd number between 1 and 50");
 
-    for (let i = 1; i <= 50; i += 2) {
-        if (userNum === i) {
-            console.log("Yikes, skipping this number: " + userNum);
-            continue;
-        }
-        console.log(oddNums + i);
+while(true) {
+
+    if (userNum % 2 === 1 && userNum <= 50 && userNum >= 1) {
+        break;
+
     }
-
 }
+
+for (let i = 1; i <= 50; i += 2) {
+    if (userNum == i) {
+        console.log("Yikes, skipping this number: " + userNum);
+        continue;
+    }
+    console.log(oddNums + i);
+}
+
