a9a06f4bce26ac6750c03e6cf1acff313cc694be
Samuel Arguello <sam.rene12@gmail.com>
2022-02-08 09:27:53 -0600

    wrote a few functions in js-warmups.js

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -10,3 +10,43 @@ function numTimesTwo(num) {
 }
 // console.log(numTimesTwo(2));
 // console.log(numTimesTwo(100));
+
+function numPlusItself(num) {
+    return num + num;
+}
+// console.log(numPlusItself(8));
+
+
+function doubleSum(num1, num2) {
+    return (num1 *= 2) + (num2 *= 2);
+}
+// console.log(doubleSum(2, 2));
+
+
+function squareRoot(num) {
+    return num**2;
+}
+// console.log(squareRoot(2));
+
+
+function strLength(str) {
+    return str.length;
+}
+// console.log(strLength("hi"))
+
+
+function twoStrings(str1, str2){
+    return (str1.length) + (str2.length);
+}
+// console.log(twoStrings("hi", "hello"));
+
+
+function evenOrOdd(num) {
+    if(num % 2 === 0) {
+        return "even";
+    } else {
+        return "odd"
+    }
+}
+// console.log(evenOrOdd(2));
+// console.log(evenOrOdd(3));
