29f3ac59fe6d12435b21c5114a874d5328217cbb
Samuel Arguello <sam.rene12@gmail.com>
2022-03-02 16:24:51 -0600

    completed dom-query-js

--- lecture notes/dom-events-lecture/dom-events-lecture.js
+++ lecture notes/dom-events-lecture/dom-events-lecture.js
@@ -1,17 +1,17 @@
 "use strict";
 
-// let button = document.querySelector("button");
+let button = document.querySelector("button");
 
 function doThisWhenTheButtonIsClicked(){
     alert("You clicked me!");
 }
 
-// button.addEventListener("click", doThisWhenTheButtonIsClicked);
+button.addEventListener("click", doThisWhenTheButtonIsClicked);
 //
 // let secondButton =document.getElementsByTagName("button")[1];
 // secondButton.onclick = doThisWhenTheButtonIsClicked;
 
-let allTheButtons = document.getElementsByTagName("button");
-for (let i = 0; i < allTheButtons.length; i++){
-    allTheButtons[i].addEventListener('click', doThisWhenTheButtonIsClicked)
-}
+// let allTheButtons = document.getElementsByTagName("button");
+// for (let i = 0; i < allTheButtons.length; i++){
+//     allTheButtons[i].addEventListener('click', doThisWhenTheButtonIsClicked)
+// }
