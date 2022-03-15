21551707a5ad3447ec4d1a21ad54c6940ba11c5b
Samuel Arguello <sam.rene12@gmail.com>
2022-03-15 09:07:53 -0500

    completed daily js warmup

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -470,7 +470,52 @@ function countNegatives(numArr){
     return total;
 }
 
-console.log(countNegatives(nums1), 1);
-console.log(countNegatives(nums2), 2);
-console.log(countNegatives(nums3), 0);
-console.log(countNegatives(nums4), 2    );
+// console.log(countNegatives(nums1), 1);
+// console.log(countNegatives(nums2), 2);
+// console.log(countNegatives(nums3), 0);
+// console.log(countNegatives(nums4), 2);
+
+// Create a function, createFirstNamesArray, that takes in an array of user objects and returns an array of first name strings. Assume that each user object has a property called 'firstName'.
+//
+//     Example...
+
+var users = [
+    {
+        firstName: 'Bob',
+        lastName: 'Smith',
+        email: 'bob@email.com'
+    },
+    {
+        firstName: 'Sally',
+        lastName: 'Smith',
+        email: 'sally@email.com'
+    },
+    {
+        firstName: 'Fred',
+        lastName: 'Smith',
+        email: 'fred@email.com'
+    },
+    {
+        firstName: 'Cathy',
+        lastName: 'Smith',
+        email: 'cathy@email.com'
+    },
+    {
+        firstName: 'Joe',
+        lastName: 'Smith',
+        email: 'joe@email.com'
+    },
+];
+
+// createFirstNamesArray(users); // returns... ['Bob', 'Sally', 'Fred', 'Cathy', 'Joe']
+
+function createFirstNamesArray(users){
+    let firstNamesArray = [];
+
+    users.forEach(function(user){
+        firstNamesArray.push(user.firstName);
+    })
+    return firstNamesArray
+}
+
+// console.log(createFirstNamesArray(users), ['Bob', 'Sally', 'Fred', 'Cathy', 'Joe'])
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,49 +1,62 @@
 "use strict";
 
-function createWeatherTables(data) {
-    let html = '';
-    for (let i = 0; i < 5; i++) {
-        let date = new Date(data.daily[i].dt * 1000).toDateString();
 
 
 
-        html += `<div class="weather-cards">
-                        <h5>${date}</h5>
-                        <i>${data.daily[i].weather.icon}</i>
-                        <p>Feels like: ${data.daily[i].feels_like.day}</p>
-                        <p>High: ${data.daily[i].temp.max}</p>
-                        <p>Low: ${data.daily[i].temp.min}</p>
-                </div>`
-    }
+let submittedLat = 29.423017;
+let submittedLong = -98.48527;
+
+
+
+
+
+// creates weather cards and displays them to the dom
+    function createWeatherTables(data) {
+        let html = '';
+
+        for (let i = 0; i < 5; i++) {
+            let date = new Date(data.daily[i].dt * 1000).toDateString();
+
+            html += `<div class="weather-cards">
+                            <h5>${date}</h5>
+                            <i>${data.daily[i].weather.icon}</i>
+                            <p>Feels like: ${data.daily[i].feels_like.day}</p>
+                            <p>High: ${data.daily[i].temp.max}</p>
+                            <p>Low: ${data.daily[i].temp.min}</p>
+                    </div>`
+        }
         return html;
-}
+    }
+
+
+// gets api data and feeds it to display function
+    $.get("https://api.openweathermap.org/data/2.5/onecall?", {
+        APPID: OPEN_WEATHER_API_TOKEN,
+        lat:    submittedLat,
+        lon:   submittedLong,
+        units: "imperial"
+
+    }).done(function(data) {
+        $("#forecast").html(createWeatherTables(data));
+    });
 
 
 
+// ========== map functionality ==========
 
-$.get("https://api.openweathermap.org/data/2.5/onecall?", {
-    APPID: OPEN_WEATHER_API_TOKEN,
-    lat:    29.423017,
-    lon:   -98.48527,
-    units: "imperial"
+mapboxgl.accessToken = MAPBOX_API_TOKEN;
 
-}).done(function(data) {
-    $("#forecast").html(createWeatherTables(data));
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-98.491142, 29.424349], // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
 
 
-
-
-
-
-
-// map functionality
-
-// mapboxgl.accessToken = MAPBOX_API_TOKEN;
-//
-// const map = new mapboxgl.Map({
-//     container: 'map', // container ID
-//     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-//     center: [-98.491142, 29.424349], // starting position [lng, lat]
-//     zoom: 10 // starting zoom
-// });
+$("button").click(function(){
+    geocode("Zion national park", MAPBOX_API_TOKEN).then(function(result) {
+        map.setCenter(result);
+        map.setZoom(10);
+    });
+})
